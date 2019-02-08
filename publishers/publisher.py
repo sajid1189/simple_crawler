d97a7e091013255a9e21607af53bdf2c711a9167
sajidur89 <sajid1189@gmail.com>
2019-02-08 13:42:39 +0100

    a hash of hte url is stored in the redis cache.

--- publishers/publisher.py
+++ publishers/publisher.py
@@ -2,32 +2,41 @@ import pika
 import os
 import sys
 import redis
+import hashlib
+import base64
 
 sys.path.append(os.path.abspath('../'))
 
 from settings import settings
 
 
-def check_and_publish(ch, method, properties, body):
+def check_and_publish(ch, method, properties, url):
     """
     A callback function that is called when a message is received by the worker. It downloads the passed url,
     parses the content for the outgoing links (urls), stores the content
     :param ch: RabbitMQ channel
     :param method:
     :param properties:
-    :param body: message body. Typically, it the url that the worker is going to download and process
+    :param url: The message body of RabbitMQ message. Typically, it is
+     the url that the worker is going to download and process
     :return: None
     """
 
     # Check if the link is already downloaded i.e.,if it exists in the global set
-    if body not in rds:
+    url_hash = _get_alphanumeric_hash(url)
+    if url_hash not in rds:
         con = pika.BlockingConnection(pika.ConnectionParameters(host=settings.DOWNLOADABLE_QUEUE_IP))
         ch = con.channel()
         ch.queue_declare(queue=settings.DOWNLOADABLE_QUEUE)
 
-        ch.basic_publish(exchange="", routing_key=settings.DOWNLOADABLE_QUEUE, body=body)
+        ch.basic_publish(exchange="", routing_key=settings.DOWNLOADABLE_QUEUE, body=url)
         con.close()
-        rds.set(body, 1)
+        rds.set(url_hash, 1)
+
+
+def _get_alphanumeric_hash(url):
+    hs = hashlib.md5(url.encode()).digest()
+    return base64.b64encode(hs)
 
 
 if __name__ == '__main__':
